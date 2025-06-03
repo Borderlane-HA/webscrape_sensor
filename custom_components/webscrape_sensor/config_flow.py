fcf329db5e8b87ceb94412d9a8ce1afa99d8eb43
Borderlane-HA <Spam-Schutz@gmx.de>
2025-06-03 15:00:56 +0200

    Update config_flow.py

--- custom_components/webscrape_sensor/config_flow.py
+++ custom_components/webscrape_sensor/config_flow.py
@@ -1,6 +1,5 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 from .const import DOMAIN
 
 class WebScrapeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -8,17 +7,18 @@ class WebScrapeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         errors = {}
+
         if user_input is not None:
+            # Hier kannst du evtl. Validierung der URL machen
             return self.async_create_entry(title=user_input["name"], data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("name"): str,
-                vol.Required("url"): str,
-                vol.Required("start_string"): str,
-                vol.Required("end_string"): str,
-                vol.Optional("unit_of_measurement", default=""): str,
-            }),
-            errors=errors,
-        )
+        data_schema = vol.Schema({
+            vol.Required("name"): str,
+            vol.Required("url"): str,
+            vol.Required("start_string"): str,
+            vol.Required("end_string"): str,
+            vol.Optional("unit_of_measurement", default=""): str,
+            vol.Optional("update_interval", default=5): int,  # Minuten
+        })
+
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)

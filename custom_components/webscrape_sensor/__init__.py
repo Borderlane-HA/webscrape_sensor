fa777bfdae27137a6aa70e14a9b8316f3a3191e5
Borderlane-HA <Spam-Schutz@gmx.de>
2025-06-13 09:11:58 +0200

    Update __init__.py

--- custom_components/webscrape_sensor/__init__.py
+++ custom_components/webscrape_sensor/__init__.py
@@ -2,7 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
